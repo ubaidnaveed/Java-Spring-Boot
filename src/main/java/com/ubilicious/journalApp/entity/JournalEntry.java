e66001534c2ce91f58e0b2833aba4c2411c70568
ubaidnaveed <ubaidub97@gmail.com>
2024-06-10 09:22:44 +0500

    Rest Api

---
+++ src/main/java/com/ubilicious/journalApp/entity/JournalEntry.java
@@ -0,0 +1,33 @@
+package com.ubilicious.journalApp.entity;
+
+public class JournalEntry {
+    private long id;
+    private String tittle;
+    private String content;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTittle() {
+        return tittle;
+    }
+
+    public void setTittle(String tittle) {
+        this.tittle = tittle;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+
+}
